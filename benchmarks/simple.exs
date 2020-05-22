4c99f961bdcd928d28c97665ae08850ed9eb4b52
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2020-05-22 13:47:08 +0200

    Benchmark with multiple sizes of input

--- benchmarks/simple.exs
+++ benchmarks/simple.exs
@@ -4,21 +4,25 @@ dividends = StreamData.integer()
 divisors  = StreamData.integer() |> StreamData.filter(&(&1 != 0))
 args =
   Stream.zip(dividends, divisors)
-  |> Stream.take(1_000)
-  |> Enum.to_list
 
 Benchee.run(
   %{
-    "Modulo.mod (body)" => fn ->
-      Enum.map(args, fn {a, b}  -> Modulo.mod(a, b) end)
+    "Modulo.mod (body)" => fn input ->
+      Enum.map(input, fn {a, b}  -> Modulo.mod(a, b) end)
     end,
-    "Modulo.mod (guard)" => fn ->
-      Enum.map(args, fn {a, b}  -> Modulo.guard_safe_mod(a, b) end)
+    "Modulo.mod (guard)" => fn input ->
+      Enum.map(input, fn {a, b}  -> Modulo.guard_safe_mod(a, b) end)
     end,
-    "rem" => fn ->
-      Enum.map(args, fn {a, b} -> rem(a, b) end)
+    "rem" => fn input ->
+      Enum.map(input, fn {a, b} -> rem(a, b) end)
     end
   },
-  time: 10,
+  time: 5,
   formatters: [{Benchee.Formatters.Console, extended_statistics: true}],
+  inputs: %{
+    "1" => Enum.take(args, 1),
+    "100" => Enum.take(args, 100),
+    "1_000" => Enum.take(args, 1_000),
+    "10_000" => Enum.take(args, 10_000),
+  }
 )
